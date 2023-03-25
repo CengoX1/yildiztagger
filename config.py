800abc70707a9989069f59858e45205c24822eb9
CengoX1 <128736010+CengoX1@users.noreply.github.com>
2023-03-25 20:58:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ from telethon import TelegramClient, events
 # Burayı gurcalama
 # 
 # 
-api_id = int(os.environ.get("APP_ID", "4528632"))
-api_hash = os.environ.get("API_HASH", "c5b85b23fba198fd7f069303d29854e0")
-bot_token = os.environ.get("TOKEN", "5686579429:AAGdpQ10GrQlPf-KGeQax18WUNYa8woEtqY")
+api_id = int(os.environ.get("APP_ID", "22692275"))
+api_hash = os.environ.get("API_HASH", "745dd42941007c780d5bd2c5e37aed71")
+bot_token = os.environ.get("TOKEN", "6056385749:AAGsncYNMoV2GPsKCI11YKHDetsHB5F2Zs4")
 
 # Telethon 
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 #
-USERNAME = os.environ.get("USERNAME", "Nazaramigeldikdersin")
-log_qrup = int(os.environ.get("LOG_QRUP", "-1001654938309"))
+USERNAME = os.environ.get("USERNAME", "Cengonuzz")
+log_qrup = int(os.environ.get("LOG_QRUP", "-1001980878821"))
 startmesaj = os.environ.get("startmesaj")
 komutlar = os.environ.get("komutlar")
 qrupstart = os.environ.get("qrupstart")
